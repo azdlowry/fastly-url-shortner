9e4f3d9daf32bdbfb3bbbf3c85991dd1b41a76a8
Andy <andy@andylowry.me.uk>
2025-06-10 14:54:51 +0100

    Add basic PUT method for adding new shortcodes

--- src/main.rs
+++ src/main.rs
@@ -28,8 +28,12 @@ fn main(req: Request) -> Result<Response, Error> {
 fn handler(req: Request) -> Result<Response, Error> {
     // Filter request methods...
     match req.get_method() {
+        &Method::PUT => {
+            return handle_put(req);
+        }
+        
         // Block requests with unexpected methods
-        &Method::POST | &Method::PUT | &Method::PATCH | &Method::DELETE => {
+        &Method::POST | &Method::PATCH | &Method::DELETE => {
             return Ok(Response::from_status(StatusCode::METHOD_NOT_ALLOWED)
                 .with_header(header::ALLOW, "GET, HEAD, PURGE")
                 .with_body_text_plain("This method is not allowed\n"))
@@ -39,6 +43,38 @@ fn handler(req: Request) -> Result<Response, Error> {
         _ => (),
     };
 
+    handle_get(req)
+}
+
+fn handle_put(mut req: Request) -> Result<Response, Error> {
+    // Ensure the request has a body
+    if !req.has_body() {
+        return Ok(Response::from_status(StatusCode::BAD_REQUEST)
+            .with_body_text_plain("PUT requests must have a body\n"));
+    }
+
+    let body = req.take_body_json::<AddRedirectRequest>()?;
+
+    // Extract the key from the path
+    let key = match req.get_path() {
+        path if path.len() == 7 && path.chars().skip(1).all(|c| c.is_ascii_alphanumeric()) => &path[1..7],
+        _ => return Ok(Response::from_status(StatusCode::BAD_REQUEST)
+            .with_body_text_plain("Invalid key format. Use /<6 alphanumeric characters>\n")),
+    };
+    let path = body.path;
+
+    // Open the KV store and insert the value
+    let store = fastly::kv_store::KVStore::open("shortner")?.expect("KVStore not found");
+    store.insert(key, path.clone())?;
+
+    log::info!("Stored {} for key {}", path, key);
+
+    Ok(Response::from_status(StatusCode::CREATED)
+        .with_content_type(mime::TEXT_PLAIN_UTF_8)
+        .with_body_text_plain(&format!("Stored value for key: {}\n", key)))
+}
+
+fn handle_get(req: Request) -> Result<Response, Error> {
     // Pattern match on the path...
     match req.get_path() {
         "/" => Ok(Response::from_status(StatusCode::OK)
@@ -76,6 +112,12 @@ fn redirect(key: &str) -> Result<Response, Error> {
         .with_body_text_plain("You have been redirected.\n"))
 }
 
+// serde type for add redirect request
+#[derive(serde::Deserialize, serde::Serialize, Debug)]
+pub struct AddRedirectRequest {
+    pub path: String,
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -83,14 +125,35 @@ mod tests {
 
     #[test]
     fn test_redirect() {
-        let req = fastly::Request::get("http://example.com/abc123");
+        test_any_redirect("abc123", "https://rustmanchester.co.uk/");
+    }
+
+    fn test_any_redirect(key: &'static str, path: &'static str) {
+        let req = fastly::Request::get(format!("http://example.com/{}", key));
         let resp = handler(req).expect("request succeeds");
         assert_eq!(resp.get_status(), StatusCode::PERMANENT_REDIRECT);
         assert_eq!(
             resp.get_header(header::LOCATION).map(|h| h.to_str().ok()),
-            Some(Some("https://rustmanchester.co.uk/"))
+            Some(Some(path))
         );
         assert_eq!(resp.get_content_type(), Some(TEXT_PLAIN_UTF_8));
         assert_eq!(resp.into_body_str(), "You have been redirected.\n");
+    }
+    
+    #[test]
+    fn test_handle_put() {
+        let req = fastly::Request::put("http://example.com/xyz789")
+            .with_body_json(&AddRedirectRequest {
+                path: "https://example.com/xyz789".to_string(),
+            })
+            .expect("valid JSON body");
+        let resp = handler(req).expect("request succeeds");
+        assert_eq!(resp.get_status(), StatusCode::CREATED);
+        assert_eq!(
+            resp.into_body_str(),
+            "Stored value for key: xyz789\n"
+        );
+
+        test_any_redirect("xyz789", "https://example.com/xyz789");
     }
 }
